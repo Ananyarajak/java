7c2907ddc415ccb154d0dd4af96a7d50c16758bd
Ananya Rajak <ananyarajak374@gmail.com>
2026-05-04 22:10:43 +0530

    Add files via upload

--- encapsulation.java
+++ encapsulation.java
@@ -1,100 +1,43 @@
-// class Atm {
-//     private int paisa;
+class Student {
 
-//     public Atm(int r) {
-//         this.paisa = r;
-//     }
+    // Private variables (data hiding)
+    private String name;
+    private int age;
 
-//     public int gatter(int a) {
-//         return paisa;
-//     }
-// }
-
-// public class encapsulation {
-//     public static void main(String[] args) {
-//         Atm s1 = new Atm(100);
-//         System.out.println(s1.gatter(20));
-//     }
-// }
-
-// class student {
-//     private String name;
-//     private int marks;
-
-//     public student(String n, int m) {
-//         this.name = "ananya";
-//         this.marks = 87;
-//     }
-
-//     public String gatter(String a) {
-//         return name;
-//     }
-
-//     public int gatter(int b) {
-//         return marks;
-//     }
-// }
-
-// public class encapsulation {
-//     public static void main(String[] args) {
-//         student s1 = new student("ananya", 87);
-//         System.out.println(s1.gatter("riya"));
-//         System.out.println(s1.gatter(87));
-//     }
-// }
-
-// class employee {
-//     private String name;
-//     private int id;
-//     private double salary;
-
-//     public employee(String n, int i, double s) {
-//         this.name = "ananya";
-//         this.id = 8;
-//         this.salary= 45000;
-//     }
-
-//     public String gatter(String a) {
-//         return name;
-//     }
-
-//     public int gatter(int i) {
-//         return id;
-//     }
-
-//     public double gatter(double s) {
-//         return salary;
-//     }
-// }
-
-// public class encapsulation {
-//     public static void main(String[] args) {
-//         employee s1 = new employee("ananya", 8, 45000);
-//         System.out.println(s1.gatter("ananya"));
-//         System.out.println(s1.gatter(8));
-//         System.out.println(s1.gatter(45000.00));
-//     }
-// }
-
-class temperature {
-    private int celcius;
-
-    public temperature(int c) {
-        this.celcius = c;
+    // Public setter methods
+    public void setName(String name) {
+        this.name = name;
     }
 
-    public int gatter() {
-        return celcius;
+    public void setAge(int age) {
+        if(age > 0) {      // validation
+            this.age = age;
+        } else {
+            System.out.println("Invalid age");
+        }
     }
 
-    public double getFahrenhite() {
-        return (celcius * 1.8) + 32;
+    // Public getter methods
+    public String getName() {
+        return name;
+    }
+
+    public int getAge() {
+        return age;
     }
 }
 
-public class encapsulation {
+public class Main {
     public static void main(String[] args) {
-        temperature t1 = new temperature(25);
-        System.out.println("temperature in fehrenhite:" + t1.getFahrenhite());
+
+        Student s1 = new Student();
+
+        // Setting values using setter
+        s1.setName("Sarthak");
+        s1.setAge(19);
+
+        // Getting values using getter
+        System.out.println("Name: " + s1.getName());
+        System.out.println("Age: " + s1.getAge());
     }
 }
